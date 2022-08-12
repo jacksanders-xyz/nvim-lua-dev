6758edc41bcf9bc468687b7e83bc6e221b61d54d
Jack Callaway Sanders <Jack.Sanders@ibm.com>
2022-08-12 17:56:14 -0400

    win bar lookin goood

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -5,5 +5,7 @@ function ColorMyPencils()
     vim.g.gruvbox_contrast_dark = 'hard'
     vim.g['airline#extensions#tabline#left_sep'] = ' '
     vim.g['airline#extensions#tabline#left_alt_sep'] = '|'
+    vim.opt.laststatus=3
+    vim.api.nvim_set_hl(0, "WinSeparator", {bg='NONE'})
 end
 ColorMyPencils()
